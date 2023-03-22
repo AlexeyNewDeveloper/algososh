6e5503d69719d0d76fbeba5168c8a45131f23819
Alexey Yanchev <yanchevalexey@yandex.ru>
2023-03-22 16:09:57 +0300

    сделал сортировку пузырьком

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -17,8 +17,17 @@ interface IColumn {
 }
 
 export const SortingPage: React.FC = () => {
+  const [method, setMethod] = React.useState<{
+    choice: boolean;
+    bubble: boolean;
+  }>({
+    choice: true,
+    bubble: false,
+  });
   const [array, setArray] = React.useState<IColumn[]>([]);
-  const [arrayChanged, setArrayChanged] = React.useState<boolean>(false);
+  const [clickSortButton, setClickSortButton] = React.useState<
+    "desc" | "asc" | boolean
+  >(false);
   const [clickNewArray, setClickNewArray] = React.useState<boolean>(false);
 
   const randomArr = () => {
@@ -37,15 +46,31 @@ export const SortingPage: React.FC = () => {
     array: Array<IColumn>,
     start: number,
     end: number,
+    typeSort: "selection" | "bubble" = "selection",
     delay: number = 500
   ) =>
     new Promise((res) =>
       setTimeout(() => {
-        array[start].changing = true;
-        array[end].changing = true;
-        if (array[end - 1].changing && array[end - 1] !== array[start]) {
-          array[end - 1].changing = false;
+        if (typeSort === "bubble") {
+          array[start].changing = true;
+          array[end].changing = true;
+          if (
+            end >= 2 &&
+            start >= 1 &&
+            array[end - 2].changing &&
+            array[start - 1].changing
+          ) {
+            array[end - 2].changing = false;
+            array[start - 1].changing = false;
+          }
+        } else {
+          array[start].changing = true;
+          array[end].changing = true;
+          if (array[end - 1].changing && array[end - 1] !== array[start]) {
+            array[end - 1].changing = false;
+          }
         }
+
         setArray([...array]);
 
         res(true);
@@ -69,6 +94,34 @@ export const SortingPage: React.FC = () => {
       return secondElement;
     }
   };
+
+  async function bubbleSort(array: IColumn[], sorting: string) {
+    const arr = [...array].map((element) => {
+      return { ...element, changing: false, modified: false };
+    });
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - i - 1; j++) {
+        if (sorting === "desc" && arr[j].number < arr[j + 1].number) {
+          await delaySwap(arr, j, j + 1, "bubble");
+          setArray([...swap(arr, j, j + 1)]);
+        } else if (sorting === "asc" && arr[j].number > arr[j + 1].number) {
+          await delaySwap(arr, j, j + 1, "bubble");
+          setArray([...swap(arr, j, j + 1)]);
+        } else {
+          await delaySwap(arr, j, j + 1, "bubble");
+        }
+      }
+      arr[arr.length - i - 1].changing = false;
+      if (arr.length - i >= 2) {
+        arr[arr.length - i - 2].changing = false;
+      }
+      arr[arr.length - i - 1].modified = true;
+    }
+
+    setArray([...arr]);
+    setClickSortButton(true);
+  }
 
   async function selectionSort(array: IColumn[], sorting: string) {
     const arr = [...array].map((element) => {
@@ -126,14 +179,22 @@ export const SortingPage: React.FC = () => {
       exchangedElement = null;
     }
     setArray([...arr]);
-
-    setArrayChanged(true);
+    setClickSortButton(true);
   }
 
   const getNewArray = () => {
     setClickNewArray(true);
     setArray(randomArr());
-    setArrayChanged(false);
+    setClickSortButton(true);
+  };
+
+  const onChangeRadioInput = (e: React.FormEvent<HTMLInputElement>): void => {
+    if (e.currentTarget.value === "choice") {
+      setMethod({ choice: true, bubble: false });
+    }
+    if (e.currentTarget.value === "bubble") {
+      setMethod({ choice: false, bubble: true });
+    }
   };
 
   return (
@@ -141,35 +202,54 @@ export const SortingPage: React.FC = () => {
       <div className={styles.top_content}>
         <RadioInput
           name="method"
+          value="choice"
           label="Выбор"
           extraClass="mr-40"
           defaultChecked
+          onChange={onChangeRadioInput}
         />
-        <RadioInput name="method" label="Пузырёк" extraClass="mr-52" />
+        <RadioInput
+          name="method"
+          value="bubble"
+          label="Пузырёк"
+          extraClass="mr-52"
+          onChange={onChangeRadioInput}
+        />
         <Button
           sorting={Direction.Ascending}
           text="По возрастанию"
           extraClass="mr-12"
           onClick={() => {
-            selectionSort(array, "asc");
+            setClickSortButton("asc");
+            if (method.choice) {
+              selectionSort(array, "asc");
+            } else if (method.bubble) {
+              bubbleSort(array, "asc");
+            }
           }}
+          isLoader={clickSortButton === "asc"}
+          disabled={clickSortButton === "desc" || !clickSortButton}
         />
         <Button
           sorting={Direction.Descending}
           text="По убыванию"
           extraClass="mr-80"
           onClick={() => {
-            if (arrayChanged) {
-              setArray([
-                ...array.map((element) => {
-                  return { ...element, changing: false, modified: false };
-                }),
-              ]);
+            setClickSortButton("desc");
+            if (method.choice) {
+              selectionSort(array, "desc");
+            } else if (method.bubble) {
+              bubbleSort(array, "desc");
             }
-            selectionSort(array, "desc");
           }}
+          isLoader={clickSortButton === "desc"}
+          disabled={clickSortButton === "asc" || !clickSortButton}
         />
-        <Button text="Новый массив" onClick={getNewArray} />
+        <Button
+          text="Новый массив"
+          onClick={getNewArray}
+          disabled={clickSortButton === "asc" || clickSortButton === "desc"}
+        />
       </div>
       {clickNewArray && (
         <div className={styles.result_box}>
